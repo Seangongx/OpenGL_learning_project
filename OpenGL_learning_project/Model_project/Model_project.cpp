3f450b87eac62cd60d19ace9fd7fe45a6115f0c1
Sean Gong <gongxun02@126.com>
2020-12-21 23:05:32 +0100

    Recently process
    Model

---
+++ OpenGL_learning_project/Model_project/Model_project.cpp
@@ -0,0 +1,58 @@
+﻿#pragma comment(lib, "legacy_stdio_definitions.lib")
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+
+#define STB_IMAGE_IMPLEMENTATION//这个定义一定要在引入stb_image.h头文件之前写
+#include "stb_image.h"
+
+#include <glm/glm.hpp>//引入图形计算库
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+
+#include "shader.h"
+#include "camera.h"
+
+#include <iostream>
+
+int main()
+{
+
+
+
+    std::cout << "Hello World!\n"; 
+}
+
+void loadModel(string path)
+{
+	Assimp::Importer import;
+	const aiScene *scene = import.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
+
+	if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
+	{
+		std::cout << "ERROR::ASSIMP::" << import.GetErrorString() << std::endl;
+		return;
+	}
+	directory = path.substr(0, path.find_last_of('/'));
+
+	processNode(scene->mRootNode, scene);
+}
+
+void processNode(aiNode *node, const aiScene *scene)
+{
+	// 处理节点所有的网格（如果有的话）
+	for (unsigned int i = 0; i < node->mNumMeshes; i++)
+	{
+		aiMesh *mesh = scene->mMeshes[node->mMeshes[i]];
+		meshes.push_back(processMesh(mesh, scene));
+	}
+	// 接下来对它的子节点重复这一过程
+	for (unsigned int i = 0; i < node->mNumChildren; i++)
+	{
+		processNode(node->mChildren[i], scene);
+	}
+}
